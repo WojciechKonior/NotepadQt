a64e3e74e740ea29906dfca2a5859829af19542a
WK5051 <wkonior@D000556.ilot.local>
2021-04-22 09:33:24 +0200

    project initialization

---
+++ notepad.h
@@ -0,0 +1,28 @@
+#ifndef NOTEPAD_H
+#define NOTEPAD_H
+
+#include <QMainWindow>
+
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QPrinter>
+#include <QPrintDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Notepad; }
+QT_END_NAMESPACE
+
+class Notepad : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Notepad(QWidget *parent = nullptr);
+    ~Notepad();
+
+private:
+    Ui::Notepad *ui;
+};
+#endif // NOTEPAD_H
